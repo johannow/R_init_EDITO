5dfa58fd560af7051b2d4ae9073cb1990c9d57a8
Jo-Hannes Nowé <148065786+johannow@users.noreply.github.com>
2024-10-08 15:41:53 +0200

    Update tools_init.sh

--- tools_init.sh
+++ tools_init.sh
@@ -1,48 +1,4 @@
-# Print a message indicating the start of the initialization
-#!/bin/bash
-
-# Print a message indicating the start of the initialization
 echo "Starting initialization..."
 
-# Install the necessary R packages
-R -e 'packages <- c("pak")'
-R -e '
-packages <- c("arrow", "tidyverse", "doParallel", "rasterVis", "mapview", 
-             "ENMeval", "dynamicSDM", "gridExtra", "pak", "raster", "spThin",
-             "BiocManager", "Rarr", "worrms", "stars", "foreach", "terra")
-
-# Function to check and install missing packages
-install_if_missing <- function(pkg){
-  if (!requireNamespace(pkg, quietly = TRUE)){
-    install.packages(pkg, dependencies = TRUE)
-  }
-}
-
-# Install CRAN packages
-invisible(sapply(packages, install_if_missing))
-
-# Install Bioconductor packages if necessary
-# Example for Bioconductor package installation
-# BiocManager::install("YourBioconductorPackage")
-
-# Optionally load the packages to verify installation
-# lapply(packages, library, character.only = TRUE)
-'
-
-# Check if R package installation was successful
-if [ $? -ne 0 ]; then
-  echo "R package installation failed. Exiting."
-  exit 1
-fi
-
-# Restart R session if RStudio is detected
-R -e '
-if (requireNamespace("rstudioapi", quietly = TRUE) && rstudioapi::isAvailable()) {
-  cat("Restarting R session in RStudio...\n")
-  rstudioapi::restartSession()
-} else {
-  cat("RStudio not detected, continuing without restarting.\n")
-}
-'
 
 
