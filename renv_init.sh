bd4fb7f66df0df51049ec597d603629cc6b2c09c
Jo-Hannes Nowé <148065786+johannow@users.noreply.github.com>
2025-10-23 09:29:42 +0200

    Update renv_init.sh

--- renv_init.sh
+++ renv_init.sh
@@ -2,6 +2,30 @@
 #!/bin/bash
 
 # Print a message indicating the start of the initialization
+echo "Install necessary system dependencies..."
+# Update package list and install dependencies
+sudo apt-get update && sudo apt-get install -y \
+    libcurl4-openssl-dev \
+    libssl-dev \
+    libxml2-dev \
+    libgit2-dev \
+    libpng-dev \
+    libgdal-dev \
+    libproj-dev \
+    libgeos-dev \
+    zlib1g-dev \
+    libudunits2-dev \
+    libsodium-dev \
+    build-essential \
+    pkg-config \
+    python3 \
+    python3-venv \
+    python3-pip
+
+# Clean up to reduce image size / disk usage
+sudo rm -rf /var/lib/apt/lists/*
+
+
 echo "Starting initialization based on renv.lock file..."
 
 R -e 'install.packages("renv", repos="https://cloud.r-project.org")'
